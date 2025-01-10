f5758beff74ae577874593e67a328e7ba8dd1f16
DeepeshPandey63 <deepesh639394@gmail.com>
2025-01-11 00:05:58 +0530

    for pulling the navbar and doctors page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [state,setState]=useState("Sign Up")
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+  const [name,setName]=useState("");
+  const onSubmitHandle=async(event)=>
+  {
+    event.preventDefault();
+  }
   return (
-    <div>Login</div>
+    <form className='min-h-[80vh] flex item-center '>
+        <div>
+          <p>{state==='Sign Up'?'Create Account':""}</p>
+        </div>
+    </form>
   )
 }
 
