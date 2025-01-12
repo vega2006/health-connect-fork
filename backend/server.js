805b712dd1564ea82c65435cb453b4c850e964ce
DeepeshPandey63 <deepesh639394@gmail.com>
2025-01-12 11:27:24 +0530

    connected my api to the mongodb and cloudinary and made modules

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+const app=express();
+const port=process.env.PORT||4000;
+connectDB();
+connectCloudinary();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+
+//api endpoints 
+app.get('/' ,(req,res)=>{
+    res.send('Api Working');
+})
+
+app.listen(port,()=>{console.log(`server started at PORT :${port}`)})
