95014ea12d61aaf0ed3b5be73e732ee47cc00e22
vega2006 <lakshyashukla984@gmail.com>
2025-01-14 21:26:02 +0530

    added addDoctors admin  page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,13 +4,27 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
 import { useContext } from 'react';
+import { Routes,Route} from 'react-router-dom';
 import Navbar from './components/Navbar'
+import Dashboard from './pages/Admin/Dashboard';
+import AddDoctor from './pages/Admin/AddDoctor';
+import AllAppointments from './pages/Admin/AllAppointments';
+import DoctorsList from './pages/Admin/DoctorsList';
 const App = () => {
   const {aToken}=useContext(AdminContext)
   return aToken? (
     <div>
       <ToastContainer/>
       <Navbar/>
+      <div className="flex items-start">
+        <Routes>
+          <Route path="/" element={<></>}/>
+          <Route path="/admin-dashboard" element={<Dashboard/>}/>
+          <Route path="/add-doctor" element={<AddDoctor/>}/>
+          <Route path="/all-appointments" element={<AllAppointments/>}/>
+          <Route path="/doctor-list" element={<DoctorsList/>}/>
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
