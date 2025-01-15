c54dc9b66fdde54e90b03a2885ef463a68a3b2c2
vega2006 <lakshyashukla984@gmail.com>
2025-01-15 09:55:07 +0530

    fixed a bug

--- frontend/src/contexts/AppContext.jsx
+++ frontend/src/contexts/AppContext.jsx
@@ -1,18 +1,33 @@
 import { createContext, useEffect, useState } from "react";
-import {doctors } from "../assets/assets_frontend/assets";
+//no more use to hardcore
+// import {doctors } from "../assets/assets_frontend/assets";
+import axios from 'axios'
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
     const currencySymbol='$';
-
-
+    const backendUrl=import.meta.env.VITE_BACKEND_URL
+    const [doctors,setDoctors]=useState([])
     const value = {
         doctors,
         currencySymbol
        
     }
-
+    const getDoctorsData=async ()=>{
+        try{
+            const {data}=await axios.get(backendUrl+'/api/doctor/list')
+            if(data.success){
+                setDoctors(data.doctors)
+            }
+        }catch(e){
+            console.log(e);
+            
+        }
+    }
+    useEffect(()=>{
+        getDoctorsData()
+    },[])
     return (
         <AppContext.Provider value={value}>
             {props.children}
