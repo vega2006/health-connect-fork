a29b58dd8a01edfba2cf14a0332d1af98f82569b
vega2006 <lakshyashukla984@gmail.com>
2025-01-15 22:42:56 +0530

    created add doctors functionality for admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,9 +10,11 @@ import MyProfile from './pages/MyProfile';
 import Appointment from './pages/Appointment';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer'
+import { ToastContainer, toast } from 'react-toastify';
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer/>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
