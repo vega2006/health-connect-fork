dd2575c3f905d7f81c6fb8106592126bd3aecf37
vega2006 <lakshyashukla984@gmail.com>
2025-02-09 13:39:27 +0530

    minor bug fixes

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ const AppContextProvider = (props) => {
 
     const calculateAge=(dob)=>{
         const today=new Date()
-        const birthDate=new Date()
+        const birthDate=new Date(dob)
         let age=today.getFullYear()-birthDate.getFullYear()
         return age
     }
